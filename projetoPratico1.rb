e0e0fc85ac30e4f259868096fc4e85245cf1a944
Riandedeus <126623299+Riandedeus@users.noreply.github.com>
2023-02-28 19:12:10 -0300

    Add files via upload

---
+++ projetoPratico1.rb
@@ -0,0 +1,9 @@
+puts "Insira seu nome:"
+nome = gets.chomp
+
+puts "Insira seu sobrenome:"
+sobrenome = gets.chomp
+puts "Digite sua idade:"
+idade = gets.chomp.to_i
+
+puts "Olá #{nome} #{sobrenome}! Seja bem vindo! sua idade é #{idade}"
